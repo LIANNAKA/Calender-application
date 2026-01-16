9b74882293e2e69ffc039db95eefa4587d2c78ac
Liannaka Dadi <liannaka.dadi10@gmail.com>
2026-01-16 22:35:11 +0530

    loaf of calender style

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function updateCalendar() {
     const now = new Date();
     const day = now.getDate();
-    const monthNames = ["january", "february", "march", "april", "may",
-        "june", "july", "august", "september", "october", "november", "december"];
-    
+    const monthNames = ["JAN", "FEB", "MAR", "APR", "MAY","JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
+
     const month = monthNames[now.getMonth()];
 
     document.getElementById('day').textContent = day;
